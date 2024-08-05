18f6968559cf5e51ab3ea11527e4ce5751112e38
Juan <anzolajuan.11@gmail.com>
2024-08-05 16:04:26 -0500

    Adding and fixing properties in Hooks

--- src/test/java/step_definition/Hooks.java
+++ src/test/java/step_definition/Hooks.java
@@ -1,15 +1,43 @@
 package step_definition;
 
-import org.junit.Before;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 
 public class Hooks {
+    Properties properties = new Properties();
+    private static ChromeDriver driver;
 
     @Before
-    public void initBrowser(){
+    public void setUp() throws IOException {
+        properties.load(new FileReader("src\\test\\resources\\configuration\\application.properties"));
         try {
-            return;
-        } catch (Exception | AssertionError e) {
-            System.out.println("Error: " + e);
+            System.setProperty("webdriver.chrome.driver", "C:\\Driver Chrome/chromedriver.exe");
+            ChromeDriver driver = new ChromeDriver();
+            driver.get(properties.getProperty("app.url"));
+            driver.manage().window().maximize();
+
+        }catch (Exception e){
+            System.out.println("Error:"+e.getMessage());
         }
+    }
+
+
+    @After
+    public void tearDown (){
+        try {
+            driver.quit();
+        }catch (Exception e){
+            System.out.println("Error:"+e.getMessage());
+        }
+    }
+
+    public static ChromeDriver getDriver(){
+        return driver;
     }
 }
