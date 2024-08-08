1fba46b25230da76edf870cc2fd1976ea45ce4f0
JosephAlvarez103 <joseph7jda@hotmail.com>
2024-08-08 12:09:04 -0500

    Changes in Hooks

--- src/test/java/step_definition/Hooks.java
+++ src/test/java/step_definition/Hooks.java
@@ -3,7 +3,9 @@ package step_definition;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.junit.Assert;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -11,33 +13,32 @@ import java.util.Properties;
 
 public class Hooks {
     Properties properties = new Properties();
-    private static ChromeDriver driver;
+    private static final ChromeDriver driver = new ChromeDriver();
 
     @Before
     public void setUp() throws IOException {
         properties.load(new FileReader("src\\test\\resources\\configuration\\application.properties"));
         try {
-            System.setProperty("webdriver.chrome.driver", "C:\\Driver Chrome/chromedriver.exe");
-            ChromeDriver driver = new ChromeDriver();
+            System.setProperty("webdriver.chrome.driver", "C:\\Driver/chromedriver.exe");
             driver.get(properties.getProperty("app.url"));
             driver.manage().window().maximize();
 
-        }catch (Exception e){
-            System.out.println("Error:"+e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Driver error:" + e.getMessage());
         }
     }
 
 
     @After
-    public void tearDown (){
+    public void tearDown() {
         try {
-            driver.quit();
-        }catch (Exception e){
-            System.out.println("Error:"+e.getMessage());
+            driver.close();
+        } catch (Exception e) {
+            System.out.println("Final error:" + e.getMessage());
         }
     }
 
-    public static ChromeDriver getDriver(){
+    public static ChromeDriver getDriver() {
         return driver;
     }
 }
